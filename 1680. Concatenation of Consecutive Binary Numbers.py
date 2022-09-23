0b89011dc367c8abc386e2e6f4a7e2a4dd2cb46a
Royal4224 <roytwang1234@gmail.com>
2022-09-22 23:47:22 -0500

    Create 1680. Concatenation of Consecutive Binary Numbers.py
    Tough to optimize (can just count the number of binary digits, don't actually have to compute the binary expansion). Also don't actually even have to count the number of binary digits, can just keep track of the powers of 2 that contain n.

---
+++ 1680. Concatenation of Consecutive Binary Numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        total_sum, _ = self.helper(n)
+
+        return total_sum
+
+    def helper(self, n):
+        if n == 1:
+            return 1, 2
+
+        prev_sum, power = self.helper(n - 1)
+
+        if n >= power:
+            power *= 2
+
+        total_sum = int((prev_sum * power + n) % 1000000007)
+
+        return total_sum, power
